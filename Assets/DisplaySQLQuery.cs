024f1e5aef0ce03b03df77611b6d20404712a838
daniel duarte <duarte.dd@gmail.com>
2015-01-29 20:06:45 -0800

    completed SQL call from list

--- Assets/DisplaySQLQuery.cs
+++ Assets/DisplaySQLQuery.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Linq;
 
 public class DisplaySQLQuery : MonoBehaviour {
 	public VaultsConnectDB initDBConnect;
@@ -9,6 +11,14 @@ public class DisplaySQLQuery : MonoBehaviour {
 
 		List< string >[] list = initDBConnect.Select ();
 	
+		Debug.Log(list[1].Find(p => p.Contains ("Alevian")));
+		string CardName = list [1].Find (p => p.Contains ("Alevian"));
+		Text cardNameBox; 
+		cardNameBox = GetComponent<Text> ();
+	cardNameBox.text = CardName;
+		
+		cardNameBox.enabled = true;
 	}
+
 
 }
